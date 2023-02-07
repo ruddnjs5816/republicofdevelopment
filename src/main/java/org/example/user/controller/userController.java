91336c59e3acb4746e59714d3caefe92232ecdeb
byeongdoohan <quden04@gmail.com>
2023-02-07 15:09:02 +0900

    feat : 프로젝트 파일 생성

--- src/main/java/org/example/user/controller/userController.java
+++ src/main/java/org/example/user/controller/userController.java
@@ -1,7 +1,13 @@
 package org.example.user.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class userController {
+
+    @PostMapping()
+
 
 
 }
