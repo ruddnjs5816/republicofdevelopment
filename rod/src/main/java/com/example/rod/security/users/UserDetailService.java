bf02ccc1a1d750c5a6b85129ffb88574ced89d33
byeongdoohan <quden04@gmail.com>
2023-02-07 21:17:48 +0900

    [refactor] dependencies 수정

--- rod/src/main/java/com/example/rod/security/users/UserDetailService.java
+++ rod/src/main/java/com/example/rod/security/users/UserDetailService.java
@@ -2,7 +2,9 @@ package com.example.rod.security.users;
 
 import com.example.rod.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
