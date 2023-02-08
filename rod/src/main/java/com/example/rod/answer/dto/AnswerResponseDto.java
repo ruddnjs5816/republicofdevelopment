e511b283508af8d9011985e35764ec0766fc765d
kyung-Park <ruddnjs5816@naver.com>
2023-02-08 20:33:59 +0900

    feat: 클래스 이름 변경, 페이징 처리

--- rod/src/main/java/com/example/rod/answer/dto/AnswerResponseDto.java
+++ rod/src/main/java/com/example/rod/answer/dto/AnswerResponseDto.java
@@ -2,22 +2,23 @@ package com.example.rod.answer.dto;
 
 import com.example.rod.comment.dto.CommentResponseDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AnswerResponseDto {
 
     private Long id;
     private String content;
-
     private int answerLike;
-
-    private List<CommentResponseDto> commentResponseDtoList;
+    private List<CommentResponseDto> commentResponseDtoList = new ArrayList<>();
 
     public AnswerResponseDto(Long id, String content, int answerLike) {
         this.id = id;
--- rod/src/main/java/com/example/rod/comment/entity/Comment.java
+++ rod/src/main/java/com/example/rod/comment/entity/Comment.java
@@ -1,32 +1,42 @@
-
 package com.example.rod.comment.entity;
 
 import com.example.rod.answer.entity.Answer;
+import com.example.rod.share.TimeStamped;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Comment {
+public class Comment extends TimeStamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String content;
 
     @ManyToOne
     @JoinColumn(name = "ANSWERS_ID")
-    private Answer answerEntity;
+    private Answer Answer;
 
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 
     public Comment(String content) {
         this.content = content;
     }
-}
 
+}
