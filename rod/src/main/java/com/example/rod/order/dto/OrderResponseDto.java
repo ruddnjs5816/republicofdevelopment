ea5e71a51e8739a61b5874c3b66cf47d2571b0e5
SOROKKIM <ksl7593@gmail.com>
2023-02-22 14:45:41 +0900

    [feat] orderResponseDto 추가

---
+++ rod/src/main/java/com/example/rod/order/dto/OrderResponseDto.java
@@ -0,0 +1,28 @@
+package com.example.rod.order.dto;
+
+import com.example.rod.order.entity.Order;
+import com.example.rod.order.entity.OrderStatus;
+import com.example.rod.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class OrderResponseDto {
+
+    private final Long orderId;
+    private final Long productId;
+    private final LocalDateTime orderDate;
+    private final OrderStatus orderStatus;
+
+    public OrderResponseDto(Order order) {
+        this.orderId = order.getId();
+        this.productId = order.getProductId();
+        this.orderDate = order.getOrderDate();
+        this.orderStatus = order.getOrderStatus();
+    }
+}
